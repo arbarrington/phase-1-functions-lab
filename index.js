72c0433fa07940cbd289ad61007eb61a3571fadf
Austin Barrington <arbarrington12@gmail.com>
2022-08-08 17:44:04 -0600

    commit tree 252850ccea31c99d3c5ef26afe5793e46c07f83c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(pickup - 42);
+}
+
+function distanceFromHqInFeet (pickup) {
+    return (Math.abs(pickup - 42))*264;
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs((start - destination)*264);
+}
+
+function calculatesFarePrice (start, destination) {
+    if (Math.abs((start - destination)*264) <= 400) {
+        return 0;
+    } else if (Math.abs((start - destination)*264) <= 2000) {
+        return .02 * (Math.abs((start - destination)*264) - 400);
+    } else if (Math.abs((start - destination)*264) <= 2500) {
+        return 25;
+    } else if (Math.abs((start - destination)*264) > 2500) {
+        return "cannot travel that far";
+    }
+}
